8ade943a861d501c2a5cdaae004b022cf9453a51
ikanit1 <nick040731@gmail.com>
2024-04-10 01:39:05 +0600

    добавил кнопку настройки на экране приветствия, но пока без функционала

--- app/src/main/java/com/example/diplomnaya/MainActivity.java
+++ app/src/main/java/com/example/diplomnaya/MainActivity.java
@@ -10,10 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView dateTextView;
+    private Button settingsButton;
     private final Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
@@ -21,9 +23,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        settingsButton = findViewById(R.id.button_settings);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettingsActivity(); // Вызываем метод для открытия экрана настроек
+            }
+        });
+    }
+
+    public void openSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class); // Создаем Intent для перехода на экран настроек
+        startActivity(intent); // Запускаем экран настроек
+    }
         // Настройка вашего пользовательского интерфейса, инициализация переменных и т.д.
 
-    }
+
+
 
     // Метод для перехода на следующую активити
     public void startNextActivity(View view) {
